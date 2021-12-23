b5b31442c4bb7303d8448aaadd5646392eee744f
Mingkai Dong <mingkaidong@gmail.com>
2021-12-24 01:27:09 +0800

    Add a patch to expose window parameters

--- Formula/emacs-plus@29.rb
+++ Formula/emacs-plus@29.rb
@@ -88,6 +88,7 @@ class EmacsPlusAT29 < EmacsBase
   local_patch "system-appearance", sha: "d6ee159839b38b6af539d7b9bdff231263e451c1fd42eec0d125318c9db8cd92"
   local_patch "poll", sha: "052eacac5b7bd86b466f9a3d18bff9357f2b97517f463a09e4c51255bdb14648" if build.with? "poll"
   local_patch "round-undecorated-frame", sha: "0e5ac1ccabb66a374fc9e8f2fb2d6e591d4c211b4aa912be280e68a846063184"
+  local_patch "expose-window-parameters", sha: "5e6fe176d44537f4a2a43a30ae78607e0b25a110243f78edf21a3dc0b6c7253a"
 
   #
   # Initialize
